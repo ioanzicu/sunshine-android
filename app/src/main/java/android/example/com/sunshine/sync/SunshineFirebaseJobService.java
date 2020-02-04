5564680c52c76ab3f688b93a9e367fbcde23082e
ioanz <ioan.zicu94@gmail.com>
2020-02-05 00:16:15 +0100

    setup FirebaseJobService

---
+++ app/src/main/java/android/example/com/sunshine/sync/SunshineFirebaseJobService.java
@@ -0,0 +1,63 @@
+package android.example.com.sunshine.sync;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import androidx.annotation.NonNull;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+import com.firebase.jobdispatcher.RetryStrategy;
+
+public class SunshineFirebaseJobService extends JobService {
+
+    private AsyncTask<Void, Void, Void> mFetchWeatherTask;
+
+    /**
+     * The entry point to Job. Implementations should offload work to another thread of
+     * execution as soon as possible.
+     * <p>
+     * This is called by the Job Dispatcher to tell us we should start our job. Keep in mind this
+     * method is run on the application's main thread, so we need to offload work to a background
+     * thread.
+     *
+     * @return whether there is more work remaining.
+     */
+
+    @Override
+    public boolean onStartJob(@NonNull final JobParameters job) {
+
+        mFetchWeatherTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Context context = getApplicationContext();
+                SunshineSyncTask.syncWeather(context);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                jobFinished(job, false);
+            }
+        };
+
+        mFetchWeatherTask.execute();
+        return true;
+    }
+
+    /**
+     * Called when the scheduling engine has decided to interrupt the execution of a running job,
+     * most likely because the runtime constraints associated with the job are no longer satisfied.
+     *
+     * @return whether the job should be retried
+     * @see Job.Builder#setRetryStrategy(RetryStrategy)
+     * @see RetryStrategy
+     */
+    @Override
+    public boolean onStopJob(@NonNull JobParameters job) {
+        if (mFetchWeatherTask != null) {
+            mFetchWeatherTask.cancel(true);
+        }
+
+        return true;
+    }
+}
